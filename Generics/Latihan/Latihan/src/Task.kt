695536869d39aaa7875561db99ee455a7f122902
Ilham Budi Prasetyo <budip.ilham@gmail.com>
2019-10-24 22:48:38 +0700

    initial commit

---
+++ Generics/Latihan/Latihan/src/Task.kt
@@ -0,0 +1,18 @@
+// Coming Soon
+fun main() {
+    val stringResult = getResult("Kotlin")
+    val intResult = getResult(100)
+
+    // TODO 2
+    println(stringResult)
+    println(intResult)
+}
+
+// TODO 1
+fun <T> getResult(args: T): Int {
+    when (args) {
+        is Int -> return args * 5
+        is String -> return args.length
+        else -> return 0
+    }
+}
